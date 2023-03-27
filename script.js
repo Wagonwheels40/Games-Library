77cd4cb7267cd0f4571964c235531014df8113c2
Wagonwheels40 <116485787+Wagonwheels40@users.noreply.github.com>
2023-03-28 09:44:17 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,45 @@
-let gamesLibrary = [
-    {
-        title: "Resident Evil",
-        genre: "Survival-Horror",
-        platform: "PC"
-    },
-    {
-        title: "Counter Strike",
-        genre: "Shooter",
-        platform: "PC"
-    }
-];
-
-let addGameBtn = document.getElementById("add-game-btn");
+let addGameBtn = document.getElementById("add-platform-btn");
 let gamesTableBody = document.getElementById("games-table-body");
+const removeButton = document.getElementById("remove-button");
+let gamesLibrary = [];
 
+addGameBtn.addEventListener("click", addGame);
 
 function addGame() {
-    let title = prompt("Enter the title of the game:");
-    let genre = prompt("Enter the genre of the game:");
-    let platform = prompt("Enter the platform of the game:");
+    let title = document.getElementById("title").value;
+    let genre = document.getElementById("genre").value;
+    let platform = document.getElementById("platform").value;
 
     let newGame = {
         title: title,
         genre: genre,
-        platform
+        platform: platform
     };
 
     gamesLibrary.push(newGame);
+
+    let newRow = document.createElement("tr");
+    let titleCell = document.createElement("td");
+    let genreCell = document.createElement("td");
+    let platformCell = document.createElement("td");
+
+    let removeButton = document.createElement("button");
+    removeButton.textContent = 'Remove';
+    removeButton.classList.add('btn', 'btn-outline-danger', 'float-end', 'remove-button');
+    removeButton.addEventListener("click", function() {
+        gamesTableBody.removeChild(newRow);
+    });
+
+    titleCell.textContent = title;
+    genreCell.textContent = genre;
+    platformCell.textContent = platform;
+
+    newRow.appendChild(titleCell);
+    newRow.appendChild(genreCell);
+    newRow.appendChild(platformCell);
+    newRow.appendChild(removeButton);
+
+    gamesTableBody.appendChild(newRow);
 }
+
+// The issue with the code is that you are trying to append the removeButton to an undefined variable newItem, which is not created in the code. You should append the removeButton to the newRow instead. Here's the corrected code: //

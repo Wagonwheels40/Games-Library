78be88a78338195aa658a7ae8f4578a1852c55ac
Wagonwheels40 <116485787+Wagonwheels40@users.noreply.github.com>
2023-03-29 09:32:38 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let addGameBtn = document.getElementById("add-platform-btn");
 let gamesTableBody = document.getElementById("games-table-body");
-const removeButton = document.getElementById("remove-button");
 let gamesLibrary = [];
 
 addGameBtn.addEventListener("click", addGame);
+
+function saveGames() {
+    localStorage.setItem("gamesLibrary", JSON.stringify(gamesLibrary));
+}
 
 function addGame() {
     let title = document.getElementById("title").value;
@@ -17,29 +20,72 @@ function addGame() {
     };
 
     gamesLibrary.push(newGame);
+    saveGames();
 
     let newRow = document.createElement("tr");
     let titleCell = document.createElement("td");
     let genreCell = document.createElement("td");
     let platformCell = document.createElement("td");
-
     let removeButton = document.createElement("button");
     removeButton.textContent = 'Remove';
     removeButton.classList.add('btn', 'btn-outline-danger', 'float-end', 'remove-button');
     removeButton.addEventListener("click", function() {
         gamesTableBody.removeChild(newRow);
+        let index = gamesLibrary.findIndex(game => game.title === titleCell.textContent);
+        gamesLibrary.splice(index, 1);
+        saveGames();
     });
 
-    titleCell.textContent = title;
-    genreCell.textContent = genre;
-    platformCell.textContent = platform;
-
+    titleCell.textContent = newGame.title;
+    genreCell.textContent = newGame.genre;
+    platformCell.textContent = newGame.platform;
     newRow.appendChild(titleCell);
     newRow.appendChild(genreCell);
     newRow.appendChild(platformCell);
     newRow.appendChild(removeButton);
-
     gamesTableBody.appendChild(newRow);
 }
 
-// The issue with the code is that you are trying to append the removeButton to an undefined variable newItem, which is not created in the code. You should append the removeButton to the newRow instead. Here's the corrected code: //
+window.addEventListener("load", function() {
+    let savedGames = JSON.parse(localStorage.getItem("gamesLibrary"));
+    if (savedGames) {
+        gamesLibrary = savedGames;
+        for (let i = 0; i < gamesLibrary.length; i++) {
+            let game = gamesLibrary[i];
+            let newRow = document.createElement("tr");
+            let titleCell = document.createElement("td");
+            let genreCell = document.createElement("td");
+            let platformCell = document.createElement("td");
+            let removeButton = document.createElement("button");
+            removeButton.textContent = 'Remove';
+            removeButton.classList.add('btn', 'btn-outline-danger', 'float-end', 'remove-button');
+            removeButton.addEventListener("click", function() {
+                gamesTableBody.removeChild(newRow);
+                let index = gamesLibrary.findIndex(game => game.title === titleCell.textContent);
+                gamesLibrary.splice(index, 1);
+                saveGames();
+            });
+
+            titleCell.textContent = game.title;
+            genreCell.textContent = game.genre;
+            platformCell.textContent = game.platform;
+            newRow.appendChild(titleCell);
+            newRow.appendChild(genreCell);
+            newRow.appendChild(platformCell);
+            newRow.appendChild(removeButton);
+            gamesTableBody.appendChild(newRow);
+        }
+    }
+});
+
+
+const removeButtons = document.querySelectorAll('.remove-button');
+removeButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const row = button.parentNode.parentNode;
+        row.remove();
+        let title = row.childNodes[0].textContent;
+        gamesLibrary = gamesLibrary.filter(game => game.title !== title);
+        saveGames();
+    });
+});

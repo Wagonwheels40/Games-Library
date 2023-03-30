d02fea8b92c20756ffc647b91fdbf99b9ab8f24b
Wagonwheels40 <116485787+Wagonwheels40@users.noreply.github.com>
2023-03-30 11:16:02 +1100

    Update script.js

--- script.js
+++ script.js
@@ -28,64 +28,67 @@ function addGame() {
     let platformCell = document.createElement("td");
     let removeButton = document.createElement("button");
     removeButton.textContent = 'Remove';
-    removeButton.classList.add('btn', 'btn-outline-danger', 'float-end', 'remove-button');
+    removeButton.classList.add('btn', 'btn-danger', 'float-end', 'remove-button');
     removeButton.addEventListener("click", function() {
         gamesTableBody.removeChild(newRow);
         let index = gamesLibrary.findIndex(game => game.title === titleCell.textContent);
         gamesLibrary.splice(index, 1);
         saveGames();
     });
+    let editButton = document.createElement("button");
+    editButton.textContent = 'Edit';
+    editButton.classList.add('btn', 'btn-danger', 'float-end', 'edit-button');
+    editButton.addEventListener("click", function() {
+        if (editButton.textContent === "Edit") {
+            // replace text with input fields
+            let newTitleInput = document.createElement("input");
+            newTitleInput.type = "text";
+            newTitleInput.value = titleCell.textContent;
+            titleCell.textContent = "";
+            titleCell.appendChild(newTitleInput);
 
+            let newGenreInput = document.createElement("input");
+            newGenreInput.type = "text";
+            newGenreInput.value = genreCell.textContent;
+            genreCell.textContent = "";
+            genreCell.appendChild(newGenreInput);
+
+            let newPlatformInput = document.createElement("input");
+            newPlatformInput.type = "text";
+            newPlatformInput.value = platformCell.textContent;
+            platformCell.textContent = "";
+            platformCell.appendChild(newPlatformInput);
+
+            editButton.textContent = "Save";
+        } else {
+            // update game object and save to localStorage
+            let newTitle = titleCell.querySelector("input").value;
+            let newGenre = genreCell.querySelector("input").value;
+            let newPlatform = platformCell.querySelector("input").value;
+            let index = gamesLibrary.findIndex(game => game.title === titleCell.textContent);
+            gamesLibrary[index].title = newTitle;
+            gamesLibrary[index].genre = newGenre;
+            gamesLibrary[index].platform = newPlatform;
+            saveGames();
+
+            // replace input fields with text
+            titleCell.textContent = newTitle;
+            genreCell.textContent = newGenre;
+            platformCell.textContent = newPlatform;
+
+            editButton.textContent = "Edit";
+        }
+    });
     titleCell.textContent = newGame.title;
     genreCell.textContent = newGame.genre;
     platformCell.textContent = newGame.platform;
     newRow.appendChild(titleCell);
     newRow.appendChild(genreCell);
     newRow.appendChild(platformCell);
+    newRow.appendChild(editButton);
     newRow.appendChild(removeButton);
+
     gamesTableBody.appendChild(newRow);
 }
 
-window.addEventListener("load", function() {
-    let savedGames = JSON.parse(localStorage.getItem("gamesLibrary"));
-    if (savedGames) {
-        gamesLibrary = savedGames;
-        for (let i = 0; i < gamesLibrary.length; i++) {
-            let game = gamesLibrary[i];
-            let newRow = document.createElement("tr");
-            let titleCell = document.createElement("td");
-            let genreCell = document.createElement("td");
-            let platformCell = document.createElement("td");
-            let removeButton = document.createElement("button");
-            removeButton.textContent = 'Remove';
-            removeButton.classList.add('btn', 'btn-outline-danger', 'float-end', 'remove-button');
-            removeButton.addEventListener("click", function() {
-                gamesTableBody.removeChild(newRow);
-                let index = gamesLibrary.findIndex(game => game.title === titleCell.textContent);
-                gamesLibrary.splice(index, 1);
-                saveGames();
-            });
-
-            titleCell.textContent = game.title;
-            genreCell.textContent = game.genre;
-            platformCell.textContent = game.platform;
-            newRow.appendChild(titleCell);
-            newRow.appendChild(genreCell);
-            newRow.appendChild(platformCell);
-            newRow.appendChild(removeButton);
-            gamesTableBody.appendChild(newRow);
-        }
-    }
-});
-
-
-const removeButtons = document.querySelectorAll('.remove-button');
-removeButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const row = button.parentNode.parentNode;
-        row.remove();
-        let title = row.childNodes[0].textContent;
-        gamesLibrary = gamesLibrary.filter(game => game.title !== title);
-        saveGames();
-    });
-});
+// save button still doesn't work //

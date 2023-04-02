825e2b820c06824a929955de1b9a70fe0e803ffb
Wagonwheels40 <116485787+Wagonwheels40@users.noreply.github.com>
2023-04-02 18:41:36 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,89 +1,80 @@
   document.addEventListener("DOMContentLoaded", function() {
-
     let addGameBtn = document.getElementById("add-platform-btn");
     let gamesTableBody = document.getElementById("games-table-body");
-    let gamesLibrary = [];
-    
+    let gamesLibrary = JSON.parse(localStorage.getItem("gamesLibrary")) || [];
+  
+    // populate table with existing data from local storage
+    gamesLibrary.forEach(function(game) {
+      let newRow = createGameRow(game);
+      gamesTableBody.appendChild(newRow);
+    });
+  
     addGameBtn.addEventListener("click", addGame);
-    
+  
+    // add event listeners for "keydown" event to input fields
+    let titleInput = document.getElementById("title");
+    let genreInput = document.getElementById("genre");
+    let platformInput = document.getElementById("platform");
+    titleInput.addEventListener("keydown", handleInputEnter);
+    genreInput.addEventListener("keydown", handleInputEnter);
+    platformInput.addEventListener("keydown", handleInputEnter);
+  
     function addGame() {
-        let title = document.getElementById("title").value;
-        let genre = document.getElementById("genre").value;
-        let platform = document.getElementById("platform").value;
-    
-        let newGame = {
-            title: title,
-            genre: genre,
-            platform: platform
-        };
-    
-        let editButton = document.createElement("button");
-        editButton.textContent = 'Edit';
-        editButton.classList.add('btn', 'btn-danger', 'float-end', 'edit-button');
-        
-        let newRow = document.createElement("tr");
-        let titleCell = document.createElement("td");
-        let genreCell = document.createElement("td");
-        let platformCell = document.createElement("td");
-        let removeButton = document.createElement("button");
-        
-        titleCell.textContent = newGame.title;
-        genreCell.textContent = newGame.genre;
-        platformCell.textContent = newGame.platform;
-        
-        removeButton.textContent = 'Remove';
-        removeButton.classList.add('btn', 'btn-danger', 'float-end', 'remove-button');
-        removeButton.addEventListener('click', function () {
-            newRow.remove();
-            let index = gamesLibrary.findIndex(game => game.title === newGame.title);
-            gamesLibrary.splice(index, 1);
-        });
-    
-        editButton.addEventListener("click", function() {
-            if (editButton.textContent === "Edit") {
-                let newTitleInput = document.createElement("input");
-                newTitleInput.type = "text";
-                newTitleInput.value = titleCell.textContent;
-                titleCell.textContent = "";
-                titleCell.appendChild(newTitleInput);
-            
-                let newGenreInput = document.createElement("input");
-                newGenreInput.type = "text";
-                newGenreInput.value = genreCell.textContent;
-                genreCell.textContent = "";
-                genreCell.appendChild(newGenreInput);
-            
-                let newPlatformInput = document.createElement("input");
-                newPlatformInput.type = "text";
-                newPlatformInput.value = platformCell.textContent;
-                platformCell.textContent = "";
-                platformCell.appendChild(newPlatformInput);
-            
-                editButton.textContent = "Save";
-            } else {
-                let newTitle = titleCell.querySelector("input").value;
-                let newGenre = genreCell.querySelector("input").value;
-                let newPlatform = platformCell.querySelector("input").value;
-                let index = gamesLibrary.findIndex(game => game.title === newGame.title);
-                gamesLibrary[index].title = newTitle;
-                gamesLibrary[index].genre = newGenre;
-                gamesLibrary[index].platform = newPlatform;
-            
-                titleCell.textContent = newTitle;
-                genreCell.textContent = newGenre;
-                platformCell.textContent = newPlatform;
-            
-                editButton.textContent = "Edit";
-            }
-        });
-        
-        newRow.appendChild(titleCell);
-        newRow.appendChild(genreCell);
-        newRow.appendChild(platformCell);
-        newRow.appendChild(editButton);
-        newRow.appendChild(removeButton);
-        
-        gamesTableBody.appendChild(newRow);
-        gamesLibrary.push(newGame);
+      let title = titleInput.value;
+      let genre = genreInput.value;
+      let platform = platformInput.value;
+  
+      let newGame = {
+        title: title,
+        genre: genre,
+        platform: platform
+      };
+  
+      let newRow = createGameRow(newGame);
+      gamesTableBody.appendChild(newRow);
+      gamesLibrary.push(newGame);
+      localStorage.setItem("gamesLibrary", JSON.stringify(gamesLibrary));
+  
+      titleInput.value = "";
+      genreInput.value = "";
+      platformInput.value = "";
     }
-});
+  
+    function handleInputEnter(event) {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        addGame();
+      }
+    }
+  
+    function createGameRow(game) {
+      let newRow = document.createElement("tr");
+      let titleCell = document.createElement("td");
+      let genreCell = document.createElement("td");
+      let platformCell = document.createElement("td");
+      let removeButton = document.createElement("button");
+  
+      titleCell.textContent = game.title;
+      genreCell.textContent = game.genre;
+      platformCell.textContent = game.platform;
+  
+      removeButton.textContent = "Remove";
+      removeButton.classList.add("btn", "btn-danger", "float-end", "remove-button");
+      removeButton.addEventListener("click", function() {
+        newRow.remove();
+        let index = gamesLibrary.findIndex(function(item) {
+          return item.title === game.title && item.genre === game.genre && item.platform === game.platform;
+        });
+        gamesLibrary.splice(index, 1);
+        localStorage.setItem("gamesLibrary", JSON.stringify(gamesLibrary));
+      });
+  
+      newRow.appendChild(titleCell);
+      newRow.appendChild(genreCell);
+      newRow.appendChild(platformCell);
+      newRow.appendChild(removeButton);
+  
+      return newRow;
+    }
+  });
+  
